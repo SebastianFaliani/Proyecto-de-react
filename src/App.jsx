eb083fc381f3c262182e7f31321edbdfc477f2fe
Sebastian Faliani <nedase@gmail.com>
2023-07-06 19:45:55 -0300

    Primer commit

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { CategoriesProvider } from "./context/CategoriesProvider";
+import MainLayout from "./layout";
+import AppRoutes from "./routes";
+
+function App() {
+    return (
+        <MainLayout>
+            <CategoriesProvider>
+                <AppRoutes />
+            </CategoriesProvider>
+        </MainLayout>
+    );
+}
+
+export default App;
